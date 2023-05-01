c68ef7277bece58b5999b2fa9de159a11d04426b
JBattishill <jbattishill91@gmail.com>
2023-05-01 10:11:36 +1000

    Attempt to save Github Repo

--- tailwind.config.js
+++ tailwind.config.js
@@ -982,10 +982,6 @@ module.exports = {
       display: ['bryant-web, sans-serif'],
       },
     extend: {
-      backgroundImage: {
-        'space': "url('/static/img/spacebg.jpg')",
-      },
-          
       colors: {
         grey:{
           950: '#333'
